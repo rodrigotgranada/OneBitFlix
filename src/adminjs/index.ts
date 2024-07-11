f8e90f73992fce216916712722c2fc06d18ff2c0
rodrigotgranada <rodrigotgranada@gmail.com>
2024-07-11 11:36:31 -0300

    Personalizando dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,7 +3,7 @@ import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
 import { database } from '../database'
 import { adminJsResources } from './resources'
-import { User } from '../models'
+import { Category, Course, Episode, User } from '../models'
 import bcrypt from 'bcrypt'
 import { locale } from './locale'
 
@@ -34,6 +34,22 @@ export const adminJs = new AdminJs({
 	      hoverBg: '#151515',
       }
     }
+  },
+  dashboard: {
+    component: AdminJs.bundle('./components/Dashboard'),
+		handler: async (req, res, context) => {
+      const courses = await Course.count()
+      const episodes = await Episode.count()
+      const category = await Category.count()
+      const standardUsers = await User.count({ where: { role: 'user' } })
+
+      res.json({
+        'Cursos': courses,
+        'Episódios': episodes,
+        'Categorias': category,
+        'Usuários': standardUsers
+      })
+    },
   }
 })
 
