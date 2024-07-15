e6f9e228d50c4c30933157dedbe87d503d9c199b
rodrigotgranada <rodrigotgranada@gmail.com>
2024-07-15 17:43:59 -0300

    Rotas protegidas com middleware

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -5,5 +5,8 @@ const secret = 'chave-jwt'
 export const jwtService = {
   signToken: (payload: string | object | Buffer, expiration: string) => {
     return jwt.sign(payload, secret, { expiresIn: expiration })
+  },
+  verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+    return jwt.verify(token, secret, callbackfn)
   }
 }
