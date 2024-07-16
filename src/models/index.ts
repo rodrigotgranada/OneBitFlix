aaa21cd6a1786c01ba83857c082d69496114b7c5
rodrigotgranada <rodrigotgranada@gmail.com>
2024-07-16 10:58:12 -0300

    Salvando cursos favotiros por cursos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 import { Category } from './Category'
 import { Course } from './Course'
 import { Episode } from './Episode'
+import { Favorite } from './Favorite';
 import { User } from './User';
 
 
@@ -8,12 +9,20 @@ Category.hasMany(Course, { as: 'courses' })
 
 Course.belongsTo(Category)
 Course.hasMany(Episode, { as: 'episodes' })
+Course.belongsToMany(User, { through: Favorite })
+Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 
 Episode.belongsTo(Course)
+Favorite.belongsTo(Course)
+Favorite.belongsTo(User)
+
+User.belongsToMany(Course, { through: Favorite })
+User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 
 export {
     Course,
     Category,
     Episode,
+    Favorite,
     User
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,8 @@ import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/courseController'
 import { episodesController } from './controllers/episodesController'
 import { authController } from './controllers/authController'
-import { ensureAuth } from './middlewares/auth'
+import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
+import { favoritesController } from './controllers/favoritesController'
 
 const router = express.Router()
 
@@ -18,6 +19,11 @@ router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
-router.get('/episodes/stream', episodesController.stream)
+router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
+
+
+router.post('/favorites', ensureAuth, favoritesController.save)
+router.get('/favorites', ensureAuth, favoritesController.save)
+router.delete('/favorites', ensureAuth, favoritesController.save)
 
 export { router }
