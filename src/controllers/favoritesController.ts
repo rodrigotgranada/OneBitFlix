c1c46b64812e382f7b4b1ba019d766bbf5debdba
rodrigotgranada <rodrigotgranada@gmail.com>
2024-07-17 11:08:11 -0300

    Excluindo um favorito

--- src/controllers/favoritesController.ts
+++ src/controllers/favoritesController.ts
@@ -3,6 +3,20 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { favoriteService } from '../services/favoriteService'
 
 export const favoritesController = {
+    // GET /favorites
+  index: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id
+
+    try {
+      const favorites = await favoriteService.findByUserId(userId)
+      return res.json(favorites)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
  // POST /favorites
   save: async (req: AuthenticatedRequest, res: Response) => {
     const userId = req.user!.id
@@ -16,5 +30,23 @@ export const favoritesController = {
         return res.status(400).json({ message: err.message })
       }
     }
+  },
+
+  //DELETE /favorites/:id
+  delete: async (req: AuthenticatedRequest, res: Response) => {
+    console.log('ENtrei')
+    const userId = req.user!.id
+    const courseId = req.params.id
+    console.log(courseId)
+
+    try {
+      await favoriteService.delete(userId, Number(courseId))
+      return res.status(204).send()
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
   }
+
 }
--- src/routes.ts
+++ src/routes.ts
@@ -22,8 +22,8 @@ router.get('/courses/:id', ensureAuth, coursesController.show)
 router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
 
 
+router.get('/favorites', ensureAuth, favoritesController.index)
 router.post('/favorites', ensureAuth, favoritesController.save)
-router.get('/favorites', ensureAuth, favoritesController.save)
-router.delete('/favorites', ensureAuth, favoritesController.save)
+router.delete('/favorites/:id', ensureAuth, favoritesController.delete)
 
 export { router }
